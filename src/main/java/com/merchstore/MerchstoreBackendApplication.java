ac2f7c2eee432d66431ad048a5da8ef878b2c5db
hisamvlad <hisamvlad@gmail.com>
2020-04-18 15:06:42 +0100

    basic structure

---
+++ src/main/java/com/merchstore/MerchstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.merchstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merchstore/MerchstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
